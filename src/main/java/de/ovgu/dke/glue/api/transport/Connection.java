1575a45fc1d74d45c2d0ca131f92d7ea1c357bb8
Stefan Haun <stefan.haun@ovgu.de>
2012-09-04 13:55:19 +0000

    API-changes: send method moved from Connection to PacketThread
    git-svn-id: https://projects.dke-research.de/svn/tools/glue/glue-core/trunk@227 75a1b36b-258a-4167-9528-f3dfdc565bb7

--- src/main/java/de/ovgu/dke/glue/api/transport/Connection.java
+++ src/main/java/de/ovgu/dke/glue/api/transport/Connection.java
@@ -3,13 +3,11 @@ package de.ovgu.dke.glue.api.transport;
 import java.net.URI;
 
 import net.jcip.annotations.ThreadSafe;
-import de.ovgu.dke.glue.api.serialization.SerializationException;
 import de.ovgu.dke.glue.api.serialization.SerializationProvider;
-import de.ovgu.dke.glue.api.serialization.Serializer;
 
 /**
- * Abstract base class representing the virtual connection between two
- * middleware implementations on different clients.
+ * Abstract base class representing the virtual connection between two matching
+ * middle-ware implementations on different clients.
  * 
  * <p>
  * Serializers are bound to a connection, i.e. each connection may have a
@@ -27,35 +25,14 @@ import de.ovgu.dke.glue.api.serialization.Serializer;
  * 
  */
 @ThreadSafe
-public abstract class Connection {
-
-	private final String connection_schema;
-
-	/**
-	 * Create a connection with a specific serialization schema.
-	 * 
-	 * @param schema
-	 *            The connection schema which may not be {@code null}
-	 * @throws NullPointerException
-	 *             if the schema is {@code null}
-	 */
-	protected Connection(final String schema) {
-		if (schema == null)
-			throw new NullPointerException(
-					"The connection schema may not be null!");
-
-		this.connection_schema = schema;
-	}
-
+public interface Connection {
 	/**
 	 * Get the connection schema for this connection, which must be set upon
 	 * creation and cannot be changed.
 	 * 
 	 * @return The serialization schema, which cannot be {@code null}
 	 */
-	public final String getConnectionSchema() {
-		return connection_schema;
-	}
+	public String getConnectionSchema();
 
 	/**
 	 * Get the connection's serialization format. The value depends on the
@@ -74,9 +51,9 @@ public abstract class Connection {
 	 * @param connection_schema
 	 *            the serialization schema for this thread
 	 * @param handler
-	 *            The packet handler to use for incoming packets. Set to {@code
-	 *            null} or {@code PacketThread.DEFAULT_HANDLER} to use the
-	 *            factory default handler for the given schema.
+	 *            The packet handler to use for incoming packets. Set to
+	 *            {@code null} or {@code PacketThread.DEFAULT_HANDLER} to use
+	 *            the factory default handler for the given schema.
 	 * @return a new packet thread
 	 * @throws TransportException
 	 *             if the thread could not be created.
@@ -94,104 +71,6 @@ public abstract class Connection {
 	 *             if the transport is not available
 	 */
 	public abstract Transport getTransport();
-
-	/**
-	 * Send a packet using this connection. The connection's transport is used.
-	 * If this method returns without an exception, the payload could be
-	 * serialized and the packet has been successfully added to the packet
-	 * queue. However, errors may still occur during the delivery process, which
-	 * will be announced over the Reporter framework.
-	 * 
-	 * @param pt
-	 *            the packet thread to use with this message, may not be {@code
-	 *            null}
-	 * @param payload
-	 *            the Payload to send with this message
-	 * @param prority
-	 *            the message priority, if supported by the transport, otherwise
-	 *            this parameter may be ignored.
-	 * @throws TransportException
-	 *             If the connection schema is unknown, the payload cannot be
-	 *             serialized or the packet could not be delivered to the send
-	 *             queue.
-	 * @throws NullPointerException
-	 *             if PacketThread pt is {@code null}
-	 * @throws IllegalArgumentException
-	 *             if the packet thread does not belong to this connection.
-	 * @throws IllegalStateException
-	 *             if the transport is not available
-	 */
-	// TODO is null-payload okay?
-	public final void send(final PacketThread pt, final Object payload,
-			final Packet.Priority priority) throws TransportException {
-		if (pt == null)
-			throw new NullPointerException(
-					"Packet thread (pt) may not be null!");
-
-		if (pt.getConnection() != this)
-			throw new IllegalArgumentException(
-					"Packet thread (pt) does not belong to this connection!");
-
-		final Transport transport = getTransport();
-		if (transport == null)
-			throw new IllegalStateException(
-					"Transport not available, connection already disposed?");
-
-		try {
-			// retrieve the schema record
-			final SchemaRecord record = SchemaRegistry.getInstance().getRecord(
-					getConnectionSchema());
-			if (record == null)
-				// TODO use a more verbose exception, e.g.
-				// UnknownTransportSchemaException or maybe
-				// SerializationException
-				throw new TransportException(
-						"The connection uses an unknown schema!");
-
-			// find the serializer according to the connection's format
-			final SerializationProvider prov = record
-					.getSerializationProvider();
-
-			final Serializer serializer = prov == null ? null : prov
-					.getSerializer(this.getSerializationFormat());
-
-			// serialize the payload
-			final Object p;
-			if (serializer != null)
-				// may throw a SeralizationException
-				p = serializer.serialize(payload);
-			else
-				p = payload;
-
-			// send the serialized payload
-			sendSerializedPayload(pt, p, priority);
-		} catch (SerializationException e) {
-			throw new TransportException("Error on payload serialization: "
-					+ e.getMessage(), e);
-		}
-	}
-
-	/**
-	 * Send a serialized packet in this thread. This method needs to be
-	 * overwritten by the transport implementation.
-	 * 
-	 * @param pt
-	 *            Packet thread to be used; caller guarantees that this
-	 *            parameter is not {@code null} and the packet thread belongs to
-	 *            this connection.
-	 * @param payload
-	 *            Serialized payload to send with this message.
-	 * @param priority
-	 *            The message priority, if supported by the transport, otherwise
-	 *            this parameter may be ignored.
-	 * @throws TransportException
-	 *             if the packet cannot be delivered to the send queue.
-	 * @throws IllegalStateException
-	 *             if the transport is not available
-	 */
-	protected abstract void sendSerializedPayload(final PacketThread pt,
-			final Object payload, final Packet.Priority priority)
-			throws TransportException;
 
 	/**
 	 * Get the thread's peer. This value is unstable as some transport
--- src/main/java/de/ovgu/dke/glue/api/transport/PacketThread.java
+++ src/main/java/de/ovgu/dke/glue/api/transport/PacketThread.java
@@ -21,6 +21,9 @@
  */
 package de.ovgu.dke.glue.api.transport;
 
+import de.ovgu.dke.glue.api.serialization.SerializationException;
+import de.ovgu.dke.glue.api.serialization.SerializationProvider;
+import de.ovgu.dke.glue.api.serialization.Serializer;
 import net.jcip.annotations.NotThreadSafe;
 
 /**
@@ -49,11 +52,28 @@ import net.jcip.annotations.NotThreadSafe;
  * 
  */
 @NotThreadSafe
-public interface PacketThread {
+public abstract class PacketThread {
 	/**
 	 * Use the default packet handler.
 	 */
 	public static PacketHandler DEFAULT_HANDLER = null;
+
+	private final Connection connection;
+
+	/**
+	 * Create a packet thread for the specified connection
+	 * 
+	 * @param connection
+	 *            Connection this packet thread belongs to
+	 * @throws NullPointerException
+	 *             if the connection parameter is @code{null}
+	 */
+	public PacketThread(final Connection connection) {
+		if (connection == null)
+			throw new NullPointerException("Connection may not be null!");
+
+		this.connection = connection;
+	}
 
 	/**
 	 * Send a packet in this thread. The thread's connection is used. If this
@@ -63,19 +83,79 @@ public interface PacketThread {
 	 * announced over the Reporter framework.
 	 * 
 	 * @param payload
-	 *            Payload to send with this message.
+	 *            the payload to send with this message
 	 * @param prority
+	 *            the message priority, if supported by the transport, otherwise
+	 *            this parameter may be ignored.
+	 * @throws TransportException
+	 *             If the connection schema is unknown, the payload cannot be
+	 *             serialized or the packet could not be delivered to the send
+	 *             queue.
+	 * @throws IllegalStateException
+	 *             if the transport is not available
+	 */
+	// TODO is null-payload okay?
+	public final void send(final Object payload, final Packet.Priority priority)
+			throws TransportException {
+		final Transport transport = getConnection().getTransport();
+		if (transport == null)
+			throw new IllegalStateException(
+					"Transport not available, connection already disposed?");
+
+		try {
+			// retrieve the schema record
+			final SchemaRecord record = SchemaRegistry.getInstance().getRecord(
+					getConnection().getConnectionSchema());
+			if (record == null)
+				// TODO use a more verbose exception, e.g.
+				// UnknownTransportSchemaException or maybe
+				// SerializationException
+				throw new TransportException(
+						"The connection uses an unknown schema!");
+
+			// find the serializer according to the connection's format
+			final SerializationProvider prov = record
+					.getSerializationProvider();
+
+			final Serializer serializer = prov == null ? null : prov
+					.getSerializer(getConnection().getSerializationFormat());
+
+			// serialize the payload
+			final Object p;
+			if (serializer != null)
+				// may throw a SeralizationException
+				p = serializer.serialize(payload);
+			else
+				p = payload;
+
+			// send the serialized payload
+			sendSerializedPayload(p, priority);
+		} catch (SerializationException e) {
+			throw new TransportException("Error on payload serialization: "
+					+ e.getMessage(), e);
+		}
+	}
+
+	/**
+	 * Send a serialized packet in this thread. This method needs to be
+	 * overwritten by the transport implementation.
+	 * 
+	 * @param pt
+	 *            Packet thread to be used; caller guarantees that this
+	 *            parameter is not {@code null} and the packet thread belongs to
+	 *            this connection.
+	 * @param payload
+	 *            Serialized payload to send with this message.
+	 * @param priority
 	 *            The message priority, if supported by the transport, otherwise
 	 *            this parameter may be ignored.
 	 * @throws TransportException
-	 *             If the payload cannot be serialized or the packet could not
-	 *             be delivered to the send queue.
+	 *             if the packet cannot be delivered to the send queue.
 	 * @throws IllegalStateException
-	 *             if this packet thread or the underlying connection/transport
-	 *             are not in a state where they can send packets.
+	 *             if the transport is not available
 	 */
-	public void send(final Object payload, final Packet.Priority priority)
-			throws TransportException;
+	protected abstract void sendSerializedPayload(final Object payload,
+			final Packet.Priority priority) throws TransportException;
 
 	/**
 	 * Dispose the packet thread.
@@ -84,7 +164,7 @@ public interface PacketThread {
 	 * longer be possible. Calling {@code send} after {@code dispose} should
 	 * result in an {@link IllegalStateException}.
 	 */
-	public void dispose();
+	public abstract void dispose();
 
 	/**
 	 * Get the connection to which this thread belongs.
@@ -94,5 +174,7 @@ public interface PacketThread {
 	 *             if the packet thread does not have a valid connection
 	 * 
 	 */
-	public Connection getConnection();
+	public final Connection getConnection() {
+		return connection;
+	}
 }
